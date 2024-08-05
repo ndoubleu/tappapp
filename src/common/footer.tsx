3fd4af8a2e05f6e49fd4c6ee013201d217c60ee2
ndoubleu <ndoubleu25@gmail.com>
2024-08-05 18:33:03 +0500

    fix: footer

--- src/common/footer.tsx
+++ src/common/footer.tsx
@@ -1,27 +1,27 @@
 export default function Footer() {
   return (
     <div className="fixed bottom-2 flex flex-col items-center w-full">
-        <div className="grid grid-cols-5 grid-rows-[1fr] bg-block rounded-[24px] border border-blockActive">
-            <div className="flex flex-col items-center w-[72px] rounded-[24px] cursor-pointer hover:bg-blockActive p-4">
+        <div className="grid grid-cols-5 grid-rows-[1fr] bg-block rounded-[24px] border border-blockActive p-[4px]">
+            <button className="flex flex-col items-center rounded-[21px] cursor-pointer hover:bg-blockActive p-4 focus:bg-blockActive aspect-square">
                 <span className="text-xl">🌽</span>
                 <span className="text-base">Farm</span>
-            </div>
-            <div className="flex flex-col items-center w-[72px] rounded-[24px] cursor-pointer hover:bg-blockActive p-4">
+            </button>
+            <button className="flex flex-col items-center rounded-[21px] cursor-pointer hover:bg-blockActive p-4 focus:bg-blockActive aspect-square">
                 <span className="text-xl">👥</span>
                 <span className="text-base">Team</span>
-            </div>
-            <div className="flex flex-col items-center w-[72px] rounded-[24px] cursor-pointer hover:bg-blockActive p-4">
+            </button>
+            <button className="flex flex-col items-center rounded-[21px] cursor-pointer hover:bg-blockActive p-4 focus:bg-blockActive aspect-square">
                 <span className="text-xl">👆</span>
                 <span className="text-base">Tap</span>
-            </div>
-            <div className="flex flex-col items-center w-[72px] rounded-[24px] cursor-pointer hover:bg-blockActive p-4">
+            </button>
+            <button className="flex flex-col items-center rounded-[21px] cursor-pointer hover:bg-blockActive p-4 focus:bg-blockActive aspect-square">
                 <span className="text-xl">💰</span>
                 <span className="text-base">Earn</span>
-            </div>
-            <div className="flex flex-col items-center w-[72px] rounded-[24px] cursor-pointer hover:bg-blockActive p-4">
+            </button>
+            <button className="flex flex-col items-center rounded-[21px] cursor-pointer hover:bg-blockActive p-4 focus:bg-blockActive aspect-square">
                 <span className="text-xl">🏅</span>
                 <span className="text-base">Top</span>
-            </div> 
+            </button> 
         </div>
     </div>
   );
