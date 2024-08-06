8c57ece8d737c3fe0ca6c0188cbd14e74b218ba9
ndoubleu <ndoubleu25@gmail.com>
2024-08-06 16:10:04 +0500

    chore: tap page demo

--- src/components/tap/index.tsx
+++ src/components/tap/index.tsx
@@ -1,7 +1,71 @@
+import tCurrency from '@images/tCurrency.png';
+import Tap from '@images/tap.png'
+import Image from 'next/image';
+import Plus from '@/assets/icons/plus';
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from 'react';
 export default function Taps() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-    <span>Tap</span>
+    <div className="relative w-full h-full flex flex-col gap-4 items-center">
+      <div className="flex flex-nowrap justify-between rounded-[12px] bg-block border border-blockActive p-4 text-sm w-full">
+        <div className='flex flex-nowrap gap-2 items-center'>
+          <Image src={tCurrency} alt="Currency" className='my-auto'/>
+          <span>15.72 GPU</span>
+        </div>
+        <div className='flex flex-nowrap justify-center bg-primary text-secondary items-center p-2 rounded-[21.5px] cursor-pointer w-[118px] h-[40px] gap-2'>
+          <Plus/>
+          <span>
+            Withdraw
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col justify-between rounded-[12px] bg-block border border-blockActive p-4 text-sm gap-4 cursor-pointer w-full">
+        <div className='flex flex-nowrap justify-between font-bold text-base'>
+          <span className=''>Finger Minig Round</span>
+          <div className='flex flex-nowrap item-center gap-2'>
+            <span>
+              56s
+            </span>
+            <IoIosArrowDown className="my-auto text-bold"/>
+          </div>
+        </div>
+        <div className="h-[8px] w-full bg-blockActive rounded-[13px]">
+          <div className="h-full animate-expand bg-primary rounded-[13px]"></div>
+        </div>
+      </div>
+      <div
+        className="absolute"
+        style={{
+          width: '437px',
+          height: '437px',
+          left: 'calc(50% - 437px/2 + 1px)',
+          top: 'calc(50% - 437px/2 + 47.5px)',
+          background: 'radial-gradient(50% 50% at 50% 50%, #2FEAB2 0%, rgba(47, 234, 178, 0) 100%)',
+          filter: 'blur(125px)'
+        }}
+      >
+      </div>
+      <div className='relative w-full h-full flex flex-col justify-between'>
+        <div className='flex flex-nowrap justify-center items-center text-4xl font-bold'>
+          <span className=''>{count}</span>
+          <span>👆</span>
+        </div>
+        <div className='flex flex-nowrap gap-2 justify-between items-center'>
+          <div className='flex flex-nowrap justify-center items-center text-4xl font-bold'>
+            <span>⚡</span>
+            <div className='flex flex-col items-end text-2xl'>
+              <span className=''>100</span>
+              <span className='text-sm opacity-60 font-normal'>/100</span>
+            </div>
+          </div>
+          <div className='flex flex-nowrap gap-2 justify-between items-center text-xl bg-black p-2 bg-opacity-25 rounded-[10px] cursor-pointer'>
+            <span>🚀</span>
+            <span>Boost</span>
+          </div>      
+        </div>
+        <Image src={Tap} alt="Tap" className='cursor-pointer absolute inset-0 m-auto' onClick={()=>{setCount(count+1)}}/>
+      </div>
   </div>
   );
 }
--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -14,7 +14,22 @@ const Teams: React.FC<TeamsProps> = ({ users }) => {
   const router = useRouter();
   return (
     <div className="flex flex-col gap-2 items-center">
-      <Image src={Squads} alt="Squads" />
+      <div className='relative'>
+        <div
+          style={{
+          position: 'absolute',
+          width: '286px',
+          height: '286px',
+          left: 'calc(50% - 286px / 2 + 0.5px)',
+          top: '0px',
+          background: 'radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0) 100%)',
+          filter: 'blur(40px)',
+        }}
+        >
+
+        </div>
+        <Image src={Squads} alt="Squads" />
+      </div>
       <span className='text-3xl text-center font-bold'>Join the squad</span>
       <span className='text-m text-center font-bold opacity-60'>These squads are waiting for you. Would you like to join one of them?</span>
       <span className='text-m text-primary font-bold cursor-pointer' onClick={()=>{router.reload()}}>Refresh</span>
@@ -22,7 +37,7 @@ const Teams: React.FC<TeamsProps> = ({ users }) => {
         {
         users &&
         users.map(user => (
-          <div key={user.id} className='flex flex-nowrap gap-2 p-4 rounded-[24px] hover:bg-blockActive'>
+          <div key={user.id} className='flex flex-nowrap gap-2 p-4 rounded-[24px] hover:bg-blockActive cursor-pointer'>
             <Avatar imagePath={user.avatar} classNames='m-auto'/>
             
             <div className="grid grid-cols-4 grid-rows-2 gap-x-4 gap-y-2 w-full text-xs">
